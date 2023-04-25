c4c404cc6436ecc4b0f96dd84ed0c0b2e98347fe
SujanAangbo <sujanaangbo100@gmail.com>
2023-04-25 21:52:44 +0545

    Added SignUpActivity

---
+++ lib/components/auth/SignUpActivity.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var email = TextEditingController();
+    var password = TextEditingController();
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(218, 245, 255, 50),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign In to Continue",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 20.0,
+                    // backgroundImage: AssetImage("fb_icon.svg"),
+                  ),
+                  CircleAvatar(
+                    radius: 20.0,
+                    // backgroundImage: AssetImage("google_icon.svg"),
+                  )
+                ],
+              ),
+              SizedBox(height: 8.0,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 100.0,
+                    height: 5.0,
+                    child: Divider(
+                      thickness: 2.0,
+                    ),
+                  ),
+                  SizedBox(width: 8.0,),
+                  Text("OR"),
+                  SizedBox(width: 8.0,),
+                  SizedBox(
+                    width: 100.0,
+                    height: 5.0,
+                    child: Divider(
+                      thickness: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8.0,),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: TextField(
+                  controller: email,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      hintText: "Enter E-mail",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: TextField(
+                  controller: password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.password),
+                      hintText: "Enter Password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print(
+                        "email: ${email.text} and password: ${password.text}");
+                  },
+                  child: Text("Sign In"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
