d47289bf4b996166c4798446260ac153e5913362
utsab kafle <utsabcodes@gmail.com>
2023-04-26 16:10:44 +0545

    added supabase_flutter library

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'components/auth/login.dart';
 import 'package:flutter/material.dart';
 import 'components/auth/SignUpActivity.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+      url: 'https://free-fund.supabase.co', anonKey: 'public-anon-key');
   runApp(const Login());
 }
