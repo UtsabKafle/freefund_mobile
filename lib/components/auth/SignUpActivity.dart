19162e00b7d98cbed62bcd37352b4330fc7bdfd5
SujanAangbo <sujanaangbo100@gmail.com>
2023-04-27 22:11:55 +0545

    Added SignUpActivity

--- lib/components/auth/SignUpActivity.dart
+++ lib/components/auth/SignUpActivity.dart
@@ -1,51 +1,50 @@
+
 import 'package:flutter/material.dart';
+import 'package:free_fund_app/component/auth/LoginActivity.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 // void main() {
 //   runApp(const MyApp());
 // }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SignUpActivity extends StatelessWidget {
+  const SignUpActivity({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     var email = TextEditingController();
     var password = TextEditingController();
+    var name = TextEditingController();
+
+
+// Get a reference your Supabase client
+    final supabase = Supabase.instance.client;
 
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        backgroundColor: Color.fromRGBO(218, 245, 255, 50),
+        backgroundColor: const Color.fromRGBO(218, 245, 255, 50),
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
+              const Text(
                 "Sign In to Continue",
                 style: TextStyle(
                   fontSize: 20.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 8.0,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+                children: const [
                   CircleAvatar(
                     radius: 20.0,
                     // backgroundImage: AssetImage("fb_icon.svg"),
@@ -56,12 +55,12 @@ class MyApp extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 8.0,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+                children: const [
                   SizedBox(
                     width: 100.0,
                     height: 5.0,
@@ -85,39 +84,80 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 8.0,
               ),
+
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: TextField(
+                  controller: name,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      hintText: "Enter Name",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
                 child: TextField(
                   controller: email,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.email),
+                      prefixIcon: const Icon(Icons.email),
                       hintText: "Enter E-mail",
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10.0))),
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
                 child: TextField(
                   controller: password,
                   obscureText: true,
                   decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.password),
+                      prefixIcon: const Icon(Icons.password),
                       hintText: "Enter Password",
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10.0))),
                 ),
               ),
               ElevatedButton(
-                  onPressed: () {
-                    print(
-                        "email: ${email.text} and password: ${password.text}");
+                  onPressed: () async {
+
+                    final AuthResponse res = await supabase.auth.signUp(
+                      email: email.text,
+                      password: password.text,
+                      data: {'username': '$name'},
+                    );
+                    final Session? session = res.session;
+                    final User? user = res.user;
+
+
+
                   },
-                  child: Text("Sign In"))
+                  child: const Text("Sign Up")
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Already have an Account, "),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginActivity()));
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Sign In",
+                      style: TextStyle(decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
