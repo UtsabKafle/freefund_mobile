be3d439ea3cecf6aaf3bef7ba9b48d55938999ee
utsab kafle <utsabcodes@gmail.com>
2023-04-27 13:45:46 +0545

    setup supabse with secret keys

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'components/auth/login.dart';
 import 'package:flutter/material.dart';
 import 'components/auth/SignUpActivity.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'secret.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
-      url: 'https://free-fund.supabase.co', anonKey: 'public-anon-key');
+      url: Secret.supabaseUrl, anonKey: Secret.supabaseAnnonKey);
   runApp(const Login());
 }
